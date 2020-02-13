7de8fdbba543821bfa5b7cb0f5b4255e0ae96282
Renaud <renaud.perchey@gmail.com>
2020-02-13 09:20:09 +0100

    avant changement httpclient true

--- src/app/core/service/movie.service.ts
+++ src/app/core/service/movie.service.ts
@@ -17,27 +17,7 @@ export class MovieService {
     private httpClient: HttpClient
   ) { }
 
-  public async allMovies() {
-    const apiRoute = `${environment.apiRoot}movie`;
-    const movies = await this.httpClient.get(environment.apiRoot).toPromise();
-    console.log(movies);
-    return movies;
-    // try {
-    //   movies = await fetch(apiRoute);
-    //   for (let property in movies) {
-    //     console.log('Property : ' + property);
-    //   }
-    //   movies.json().then((moviesList) => {
-    //     console.log('List : ' + JSON.stringify(moviesList));
-    //   })
-    //   console.log(`Movies : ${movies.json()}`);
-    // } catch (error) {
-    // }
-
-    // const movies: Promise<Response> = await fetch(apiRoute);
-    console.log(`Movies : ${JSON.stringify(movies)}`);
-  }
-
+  
   public all(): Observable<Movie[]> {
     const apiRoute = `${environment.apiRoot}movie`;
     return this.httpClient.get<Movie[]>(
